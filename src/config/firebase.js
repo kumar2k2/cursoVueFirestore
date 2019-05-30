cb166bc28738e5d7c399a88f0a42c2c52b3fa33f
alberto ruiz <alberto.ruiz@servinformacion.com>
2019-05-30 16:47:45 -0500

    first commit

---
+++ src/config/firebase.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyCVAlnbNVHD-CgyZwD_6fBWMtpuKgYtehM",
+    authDomain: "cursovuefirebase-6fa64.firebaseapp.com",
+    databaseURL: "https://cursovuefirebase-6fa64.firebaseio.com",
+    projectId: "cursovuefirebase-6fa64",
+    storageBucket: "cursovuefirebase-6fa64.appspot.com",
+    messagingSenderId: "762867436690",
+    appId: "1:762867436690:web:a27b3e1da1274859"
+  };
---
+++ src/main.js
@@ -0,0 +1,26 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+
+// ------------- configuracion de firebase y firestore
+
+import firebase from 'firebase' // se importa firebase
+import 'firebase/firestore' // se importa todo lo necesario para trabajar con firestore
+import firebaseconfig from './config/firebase' // se importa la configuracion de firebase del archivo creado en config
+firebase.initializeApp(firebaseconfig) // se inicializa firebase
+export const db=firebase.firestore() // exportacion de la variable db = database 
+
+// ------------- configuracion de firebase y firestore
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
