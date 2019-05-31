df665361a90e2f91f3faa750a8f189b986c0cd44
alberto ruiz <alberto.ruiz@servinformacion.com>
2019-05-31 18:02:55 -0500

    viernes 1

--- src/navigations/guest.vue
+++ src/navigations/guest.vue
@@ -14,7 +14,56 @@
           Vuejs 2 Firestore
         </v-btn>
       </v-toolbar-title>
+      <v-spacer/>
+      <v-toolbar-items>
+        <v-btn flat to='/login'>{{ $t('navigation.login') }}</v-btn>
+        <v-btn flat to='/register'>{{ $t('navigation.register') }}</v-btn>
+      </v-toolbar-items>
     </v-toolbar>  
+
+
+    <v-navigation-drawer
+    overlay
+    v-model='drawer'
+    absolute
+    value=''
+    >
+    <v-list class='pt-0' dense>
+      <v-divider/>
+      <v-list-tile v-for='item in items' :key='item.title' @click='$router.push(item.to)'>
+
+        <v-list-tile-action>
+          <v-icon>
+            {{ item.icon }}
+          </v-icon>
+        </v-list-tile-action>
+
+        <v-list-tile-content>
+          <v-list-tile-tile>
+            {{ item.title }}
+          </v-list-tile-tile>
+        </v-list-tile-content>
+
+      </v-list-tile>  
+
+      <v-divider/>
+
+      <v-list-tile @click='togglerDrawer'
+
+        <v-list-tile-action>
+          <v-icon>
+            arrow_back
+          </v-icon>
+        </v-list-tile-action>
+
+        <v-list-tile-content>
+          <v-list-tile-tile>
+            {{ $t('navigation.close_menu') }}
+          </v-list-tile-tile>
+        </v-list-tile-content>
+
+    </v-list> 
+    </v-navigation-drawer>
   </div>
 </template>
 
